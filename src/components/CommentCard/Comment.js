73cb869bfb89369e5fa1488407a6885cd54fce51
Ggodlyy <nikogerenski@abv.bg>
2022-09-16 11:46:05 +0300

    added avatar for comments

--- src/components/CommentCard/Comment.js
+++ src/components/CommentCard/Comment.js
@@ -1,9 +1,15 @@
 import "./Comment.scss";
+import { Avatar, Stack } from "@mui/material";
+import { orange } from "@mui/material/colors";
 
 const Comment = ({ commentText, createdAt, owner }) => {
+
   return (
     <div className="comment-dipslay-info">
-      <h3>{owner.username}  <span className="comment-date">{createdAt}</span></h3>
+      <Avatar sx={{ bgcolor: orange[500] }}>{owner.username[0]}</Avatar>
+      <h3>
+        {owner.username} <span className="comment-date">{createdAt}</span>
+      </h3>
       <p>{commentText}</p>
     </div>
   );
