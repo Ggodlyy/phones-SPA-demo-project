3b97b259148eefb734f2fb8a73f83902080eab9f
Ggodlyy <nikogerenski@abv.bg>
2022-09-16 14:26:41 +0300

    adjust comment styling

--- src/components/CommentCard/Comment.js
+++ src/components/CommentCard/Comment.js
@@ -5,15 +5,22 @@ import { NavLink } from "react-router-dom";
 
 const Comment = ({ commentText, createdAt, owner }) => {
   return (
-    <div className="comment-dipslay-info">
-      <NavLink to={`/profiles/${owner.username}`} className={'avatar-link'}>
-        <Avatar sx={{ bgcolor: orange[500] }}>{owner.username[0]}</Avatar>
-      </NavLink>
-      <h3>
-        {owner.username} <span className="comment-date">{createdAt}</span>
-      </h3>
-      <p>{commentText}</p>
-    </div>
+    <article className="comment">
+      <div className="comment-display">
+        <NavLink to={`/profiles/${owner.username}`} className={"avatar-link"}>
+          <Avatar sx={{ bgcolor: orange[500] }}>
+            {owner.username[0].toUpperCase()}
+          </Avatar>
+        </NavLink>
+      </div>
+
+      <div className="comment-dipslay-info">
+        <h3>
+          {owner.username} <span className="comment-date">{createdAt}</span>
+        </h3>
+        <p>{commentText}</p>
+      </div>
+    </article>
   );
 };
 
